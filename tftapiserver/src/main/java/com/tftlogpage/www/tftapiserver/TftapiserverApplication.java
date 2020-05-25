8dcaf3ed26adcf0e8047a954a86ad1c6473888ad
weightle55 <metalicboots@gmail.com>
2020-05-25 21:16:31 +0900

    first commit

---
+++ tftapiserver/src/main/java/com/tftlogpage/www/tftapiserver/TftapiserverApplication.java
@@ -0,0 +1,13 @@
+package com.tftlogpage.www.tftapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftapiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftapiserverApplication.class, args);
+	}
+
+}
